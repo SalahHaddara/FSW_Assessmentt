f55a128c62cb2e488c21494f9674c5e7ea51671d
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-02 11:58:43 +0200

    create middleware

---
+++ laravel/app/Http/Middleware/TrackApiRequests.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class TrackApiRequests
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response) $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $response = $next($request);
+
+        User::find($request->user_id)->increment('requests_num');
+
+        return $response;
+    }
+}
