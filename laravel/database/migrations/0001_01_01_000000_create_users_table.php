932f7472b109849fed6f02f87cf64337eaac7e34
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-02 11:42:03 +0200

    add a requests_num column to user

--- laravel/database/migrations/0001_01_01_000000_create_users_table.php
+++ laravel/database/migrations/0001_01_01_000000_create_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -18,6 +17,7 @@ return new class extends Migration
             $password->string("password");
             $table->timestamps();
             $table->string('name');
+            $table->integer('requests_num')->default(0);
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
