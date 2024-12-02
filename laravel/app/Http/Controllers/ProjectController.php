0b634480b350f6be8920cbb1f77fab5bb17b7908
Salah Haddara <Salahhaddara02@gmail.com>
2024-12-02 11:30:03 +0200

    add show to ProjectController

--- laravel/app/Http/Controllers/ProjectController.php
+++ laravel/app/Http/Controllers/ProjectController.php
@@ -37,7 +37,8 @@ class ProjectController extends Controller
      */
     public function show(string $id)
     {
-        //
+        $project = Project::findOrFail($id);
+        return response()->json($project);
     }
 
     /**
